240097c38cb589f4b15262b27db479073b387fee
kembasa <44671533+kembasa@users.noreply.github.com>
2018-11-15 08:06:03 -0800

    update birth

---
+++ Day1/birth.py
@@ -0,0 +1,16 @@
+year=int(input("please enter your year of birth\n"))
+age=int(input("please enter your age\n"))
+#def Year():
+   
+if age<18:
+   print("You are a Minor")
+else:
+    if 18<age<36:
+        print("You are a Youth")
+    else:
+        if age>=36:
+            print("You are an Elder")   
+        
+
+
+#Year()
